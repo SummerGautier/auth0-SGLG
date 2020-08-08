91dc563bfa8e051af7f205b7d91b9aff4fb216f0
Summer Gautier <summergautier@gmail.com>
2020-08-08 11:30:08 -0700

    added environment variables

--- sglg-communications/src/components/Chat/Chat.js
+++ sglg-communications/src/components/Chat/Chat.js
@@ -1,37 +1,32 @@
 import React from 'react';
+import DigestFetch from "digest-fetch"
 import { v4 as uuidv4 } from 'uuid';
-import {AxiosDigest} from "axios-digest"
-import axios from "axios"
+
 const getResponse = (props) => {
-    var data = JSON.stringify({"name":"chatroom-"+uuidv4(),"orgId":props.organizationId});
-
-    var config = {
-        method: 'POST',
-        url: 'https://cloud.mongodb.com/api/atlas/v1.0/groups',
-        headers: { 
-            'Content-Type': 'application/json'
-        },
-        data : data,
-        auth: {
-            username: props.public,
-            password: props.private
-        },
-    };
-
-    axios(config).then(function (response) {
-        console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-        console.log("")
-        console.log(error);
-    });
+    const client = new DigestFetch(props.public, props.private)
+    client.fetch("https://cloud.mongodb.com/api/atlas/v1.0/groups", 
+        {
+            method:'POST',
+            body: JSON.stringify(
+                {
+                    "name":"chatroom-"+uuidv4(),
+                    "orgId":props.organizationId
+                }
+            ),
+            headers: {'Access-Control-Allow-Origin': '*'},
+            mode:"no-cors"
+        }
+    )
+    .then(resp=>resp.json())
+    .then(data=>data)
+    .catch(e=>e)
 }
 
 const Chat = (props) =>{
     const response = getResponse(props)
     return (
         <div>
-            Response: {response}
+            Respone: {response}
         </div>
     )
 }
--- sglg-communications/src/components/Chat/index.stories.js
+++ sglg-communications/src/components/Chat/index.stories.js
@@ -4,7 +4,7 @@ import Chat from './Chat';
 export default { title: 'Chatroom' };
 
 export const chat = () => <Chat 
-        organizationId="5d86705acf09a2dcdf6a5ab4"
-        public="GUSGUIEP"
-        private="b2a8adae-5ddf-4d27-a80c-a3bd8982f1ba"
+        organizationId={process.env.ORGANIZATION_ID}
+        public={process.env.PUBLIC_KEY}
+        private={process.env.PRIVATE_KEY}
      />;
