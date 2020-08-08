2fa4f6483bb717d743c68f9ba8dbc07c46a32292
Summer Gautier <summergautier@gmail.com>
2020-08-07 17:21:01 -0700

    updating packages

--- sglg-communications/src/components/Chat/Chat.js
+++ sglg-communications/src/components/Chat/Chat.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import DigestFetch from "digest-fetch"
 import { v4 as uuidv4 } from 'uuid';
-
+import {AxiosDigest} from "axios-digest"
+import axios from "axios"
 const getResponse = (props) => {
-    const client = new DigestFetch(props.public, props.private)
-    client.fetch("https://cloud.mongodb.com/api/atlas/v1.0/groups", 
-        {
-            method:'POST',
-            body: JSON.stringify(
-                {
-                    "name":"chatroom-"+uuidv4(),
-                    "orgId":props.organizationId
-                }
-            )
-        }
-    )
-    .then(resp=>resp.json())
-    .then(data=>data)
-    .catch(e=>e)
+    var data = JSON.stringify({"name":"chatroom-"+uuidv4(),"orgId":props.organizationId});
+
+    var config = {
+        method: 'POST',
+        url: 'https://cloud.mongodb.com/api/atlas/v1.0/groups',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data : data,
+        auth: {
+            username: props.public,
+            password: props.private
+        },
+    };
+
+    axios(config).then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log("")
+        console.log(error);
+    });
 }
 
 const Chat = (props) =>{
     const response = getResponse(props)
     return (
         <div>
-            Respone: {response}
+            Response: {response}
         </div>
     )
 }
