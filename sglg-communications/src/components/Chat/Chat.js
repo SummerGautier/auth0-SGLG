f1895716fa2bd08503894aefa1caa393fe313e1a
Linda <linda@semprehealth.com>
2020-08-08 22:58:19 -0700

    Type a country name, get Covid-19 stats from covid19api.com for that country

--- sglg-communications/src/components/Chat/Chat.js
+++ sglg-communications/src/components/Chat/Chat.js
@@ -110,14 +110,25 @@ const Chat = (props) => {
   const [messageHistoryStore, setMessageHistoryStore] = useState([])
   const [inputMessage, setInputMessage] = useState("")
   const [covid19Facts, setCovid19Facts] = useState([]);
+  const [covid19CountryIndexes, setCovid19CountryIndexes] = useState({});
+
+  function countriesFrom(data) {
+    let countriesKeys = {}
+    let idx = 0
+    for (let country of data['Countries']) {
+      countriesKeys[country['Country']] = idx
+      idx += 1
+    }
+    return countriesKeys
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         "https://api.covid19api.com/summary"
       );
-
-      setCovid19Facts(result.data);
+      setCovid19Facts(result.data)
+      setCovid19CountryIndexes(countriesFrom(result.data))
     };
 
     fetchData();
@@ -160,27 +171,44 @@ const Chat = (props) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+
+  function covid19Message(countryName) {
+    const countryData = covid19Facts['Countries'][covid19CountryIndexes[countryName]]
+    const newConfirmed = countryData['NewConfirmed']
+    return `Did you know... on ${covid19Facts['Date']} there are ${newConfirmed} cases in ${countryName}`
+  }
+
+  function covidMessage(inputMessage, timestamp) {
+    const requestedCountries = Object.keys(covid19CountryIndexes).filter(countryName => inputMessage.includes(countryName))
+    if (requestedCountries.length > 0) {
+      return {
+        content: covid19Message(requestedCountries[0]),
+        timestamp: timestamp + 1,
+        from: "you"
+      }
+    } else {
+      return {
+        content: `Ask me about a country and I'll tell you a Covid-19 statistic`,
+        timestamp: timestamp + 1,
+        from: "you"
+      }
+    }
+  }
+
   function addMessage(inputMessage) {
     const timestamp = messageHistoryStore.length
     setInputMessage("")
+    const covidFact = covidMessage(inputMessage, timestamp)
     setMessageHistoryStore([...messageHistoryStore, {
-      content: inputMessage,
-      timestamp: timestamp,
-      from: "me"
-    }, {
-      content: covid19Message(),
-      timestamp: timestamp + 2,
-      from: "you"
-    }])
+        content: inputMessage,
+        timestamp: timestamp,
+        from: "me"
+      },
+        covidFact
+      ]
+    )
   }
 
-  function covid19Message() {
-    const countryPosition = getRandomInt(0, covid19Facts['Countries'].length)
-    const countryData = covid19Facts['Countries'][countryPosition]
-    const newConfirmed = countryData['NewConfirmed']
-    const countryName = countryData['Country']
-    return `Did you know... on ${covid19Facts['Date']} there are ${newConfirmed} cases in ${countryName}`
-  }
 
   const handleSubmit = (e) => {
     if (inputMessage.trim() !== "") {
